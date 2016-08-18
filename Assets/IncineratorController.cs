8278da07d27b2bae7a3235f0f3a375dc0452f17b
Fred Sauer <fredsa@gmail.com>
2016-08-17 21:52:10 -0700

    add incinterator

---
+++ Assets/IncineratorController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncineratorController : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider other)
+	{
+		Destroy (other.gameObject);
+	}
+}
